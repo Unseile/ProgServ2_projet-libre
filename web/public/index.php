ad9422c3b4903af371905a899269f506e10dd2f4
rochatheigvd <yannis.rochat@hes-so.ch>
2025-10-22 13:37:52 +0200

    Adding example to index

--- web/public/index.php
+++ web/public/index.php
@@ -2,6 +2,20 @@
 
 //Page d'accueil / liste de tous les cours
 
-include 'includes/header.php';
+include 'includes/header.php'; ?>
 
-include 'includes/footer.php';
+<h2>Ceci est le contenu du site</h2>
+
+<a href="" class="coursebox">
+    <h2 class="title">Cours d'appui en vue du 2eme examen</h2>
+    <p class="shortdescr">Cours d'appui en mathématiques pour préparer le 2eme examen de mathématiques de l'année académique 2025-2026.</p>
+    <div class="attributes">
+        <p class="price"></p>
+        <p class="subject">BaseMath2</p>
+        <p class="startdatetime">12 décembre 2025 à 15:00</p>
+        <p class="enddatetime">12 décembre 2025 à 17:00</p>
+        <p class="duration">2h</p>
+    </div>
+</a>
+
+<?php include 'includes/footer.php'; ?>
