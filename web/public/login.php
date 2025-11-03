5c57aee9e1f99724bb386211c841390a69a0bc62
Unseile <lea.haeberli03@gmail.com>
2025-11-03 17:48:05 +0100

    signin texte

--- web/public/login.php
+++ web/public/login.php
@@ -6,3 +6,35 @@ use Includes\Header, Includes\Footer;
 ?>
 <?= Header::content(); ?>
 <?= Footer::content(); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+</head>
+<body>
+
+<h2>Connexion</h2>
+
+<form action="managerLogin.php" method="POST">
+    <label>Nom:</label>
+    <input type="text" name="lastName" required><br><br>
+    
+    <label>Prénom:</label>
+    <input type="text" name="firstName" required><br><br>
+    
+    <label>Nom d'utilisateur:</label>
+    <input type="text" name="username" required><br><br>
+
+    <label>Mot de passe:</label>
+    <input type="password" name="password" required><br><br>
+
+    <label>Mail:</label>
+    <input type="email" name="email" required><br><br>
+
+    <input type="submit" value="Se connecter">
+</form>
+
+</body>
+</html>
