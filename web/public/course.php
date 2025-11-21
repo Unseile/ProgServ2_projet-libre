58efda8078b97cbb9e3f4090557c719f5a9ea3a3
Unseile <lea.haeberli03@gmail.com>
2025-11-21 13:46:46 +0100

    Avancement logout et course

--- web/public/course.php
+++ web/public/course.php
@@ -5,3 +5,15 @@ require_once __DIR__ . '/../src/Config/autoloader.php';
 ?>
 <?php include __DIR__ . '/../src/includes/header.php'; ?>
 <?php include __DIR__ . '/../src/includes/footer.php'; ?>
+
+
+<h2 class="title"><?= $course->getTitle(true) ?></h2>
+<p class="shortdescr"><?= $course->getDescr(true) ?></p>
+<div class="attributes">
+    <p class="teacher"><?= $homeContent["teacher"] ?>: <?= $course->getTeacherFirstname(true) . " " . $course->getTeacherLastname(true) ?></p>
+    <p class="subject"><?= $homeContent["subject"] ?>: <?= $course->getSubject(true) ?></p>
+    <p class="startdatetime"><?= $homeContent["on"] ?>: <?= htmlspecialchars(date('d M', strtotime($course->getStartDatetime()))) ?> <?= $homeContent["at"] ?> <?= htmlspecialchars(date('G', strtotime($course->getStartDatetime()))) ?> <?= $homeContent["hour"] ?> <?= htmlspecialchars(date('i', strtotime($course->getStartDatetime()))) ?> </p>
+    <p class="subscriptions"><?= $homeContent["subscriptions"] ?>: <?= $course->getSubStudents(true) ?></p>
+</div>
+<button>S'inscrire à ce cours</button>
+<button>Se désinscrire de ce cours</button>
--- web/public/logout.php
+++ web/public/logout.php
@@ -1,12 +1,16 @@
 <?php
 
+session_destroy();
+
+
 //Page de déconnexion
 require_once __DIR__ . '/../src/Config/autoloader.php';
 ?>
 <?php include __DIR__ . '/../src/includes/footer.php'; ?>
 <?php include __DIR__ . '/../src/includes/header.php'; ?>
 
-<form action="logout.php" method="POST">
+<form action="index.php" method="POST">
     <h2><? $languageLogout = $language->getContent($lang, 'logout')['deconnexion'] ?></h2>
-   <p><? $languageLogout['deconnexionText']?></p>  
+    <p><? $languageLogout['deconnexionText'] ?></p>
+    <button type="submit">Ok</button>
 </form>
