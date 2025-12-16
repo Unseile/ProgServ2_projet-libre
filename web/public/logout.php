cfdeb6d9bb3c74076c78be7765d4bc84af060181
Keedjud <inoewenger@gmail.com>
2025-12-16 12:36:25 +0100

    Update redirect Logout

--- web/public/logout.php
+++ web/public/logout.php
@@ -5,15 +5,6 @@ $_SESSION = [];
 
 //Page de déconnexion
 require_once __DIR__ . '/../src/Config/autoloader.php';
-include __DIR__ . '/../src/includes/header.php';
-
-$languageLogout = $language->getContent($lang, 'logout')
+header('Location: login.php');
+exit();
 ?>
-
-<form action="index.php" method="POST">
-    <h2><?= $languageLogout['deconnexion'] ?></h2>
-    <p><?= $languageLogout['deconnexionText'] ?></p>
-    <button type="submit">Ok</button>
-</form>
-
-<?php include __DIR__ . '/../src/includes/footer.php';?>
