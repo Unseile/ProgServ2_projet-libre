748d64d26033ec21a7dc642470e1d68d032b656d
miniyankey <yannisrochat@gmail.com>
2025-12-18 11:37:57 +0100

    fixing header

--- web/src/Includes/Header.php
+++ web/src/Includes/Header.php
@@ -24,7 +24,7 @@ $headerContent = $language->getContent($lang, "header");
             <ul>
                 <li><a href="index.php"><?= $headerContent["home"] ?></a></li>
                 <li><a href="profile.php"><?= $headerContent["profile"] ?></a></li>
-                <?php if (($_SESSION['isTeacher'] ?? 0) == 1) { ?>
+                <?php if ($_SESSION['isTeacher']) { ?>
                     <li><a href="create_course.php"><?= $headerContent["create-course"] ?></a></li>
                 <?php } ?>
                 <?php if (!isset($_SESSION['username'])) { ?>
